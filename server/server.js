e45f387fe84f0a701e351116f4ea2e9e61d93af6
robn <robnastoff@gmail.com>
2018-08-31 16:12:35 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -6,15 +6,19 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
 
 app.listen(port, () => {
   console.log("Server is up!");
