16da938180425cff92a8fd5402815dea78a410a1
robn <robnastoff@gmail.com>
2018-08-31 16:10:20 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -10,6 +10,12 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.listen(port, () => {
   console.log("Server is up!");
 });
