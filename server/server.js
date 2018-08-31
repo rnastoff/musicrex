6c5439ac39f5d2e898fc8412f4ef70ad3e820d5f
robn <robnastoff@gmail.com>
2018-08-31 16:18:32 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -1,19 +1,13 @@
 const path = require('path');
 const express = require('express');
+var cors = require('cors');
 const app = express();
 const publicPath = path.join(__dirname, "..", "public");
 const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
-
+app.use(cors());
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
