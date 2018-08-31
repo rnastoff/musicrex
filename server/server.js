57cbcd57402f90782a9bad245fa29368b972d3f7
robn <robnastoff@gmail.com>
2018-08-31 16:22:25 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -9,6 +9,15 @@ app.use(express.static(publicPath));
 
 app.use(cors());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://musicrex.herokuapp.com/');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
