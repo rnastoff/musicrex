ffa911d0aebdebcc971d17a74b18cbccb14ef6bd
robn <robnastoff@gmail.com>
2018-08-31 17:57:28 -0500

    fix

--- server/server.js
+++ server/server.js
@@ -9,32 +9,6 @@ app.use(express.static(publicPath));
 
 
 
-
-var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-    // intercept OPTIONS method
-    if ('OPTIONS' == req.method) {
-      res.send(200);
-    }
-    else {
-      next();
-    }
-};
-
-
-app.configure(function () {
-  app.use(allowCrossDomain);
-  app.use(express.static(publicPath));
-});
-
-
-
-
-
-
 app.get('*', (req, res) => {
   res.sendFile(path.join(publicPath, 'index.html'));
 });
--- src/components/MusicRex.js
+++ src/components/MusicRex.js
@@ -14,9 +14,11 @@ class Musicrex extends React.Component {
     };
   }
 
+  //https://tastedive.com/api/similar?q=${band}&limit=${numberOfResults}&info=1&k=308154-Test-7CWGEKQA
+
   getRecommendations = (band) => {
     const numberOfResults = 50;
-    fetch(`https://tastedive.com/api/similar?q=${band}&limit=${numberOfResults}&info=1&k=308154-Test-7CWGEKQA`)
+    fetch(`https://cors-anywhere.herokuapp.com/https://tastedive.com/api/similar?q=${band}&limit=${numberOfResults}&info=1&k=308154-Test-7CWGEKQA`)
       .then(response => response.json())
       .then(data => {
         if (data.Similar.Info[0].Type === "unknown") {
